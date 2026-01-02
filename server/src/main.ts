a2361b63cf932d15feacb8f5d9ac06d2f509c455
ruslan <r1tu@yandex.ru>
2026-01-03 00:12:23 +0300

    cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,6 +4,10 @@ import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.enableCors({
+    origin: 'https://autotochka.vercel.app',
+    credentials: true,
+  })
   app.useGlobalPipes(new ValidationPipe({ transform: true }))
   await app.listen(process.env.PORT ?? 3000)
 }
