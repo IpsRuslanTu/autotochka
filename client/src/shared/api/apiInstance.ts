8971a8673cdfba036b470a3535e4ce0569e1efed
ruslan <r1tu@yandex.ru>
2026-01-02 23:52:18 +0300

    VITE_API_BASE_URL

--- client/src/shared/api/apiInstance.ts
+++ client/src/shared/api/apiInstance.ts
@@ -1,16 +1,16 @@
 import axios, { type AxiosRequestConfig } from 'axios'
 
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
+
 export const axiosInstance = axios.create({
+  baseURL: API_BASE_URL,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
   },
 })
 
-export const createInstance = async <T>(
-  config: AxiosRequestConfig,
-  options?: AxiosRequestConfig
-): Promise<T> => {
+export const createInstance = async <T>(config: AxiosRequestConfig, options?: AxiosRequestConfig): Promise<T> => {
   const r = await axiosInstance({
     ...config,
     ...options,
